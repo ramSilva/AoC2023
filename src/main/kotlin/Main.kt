aaed493615c92033918cd9e46ed3ff20a83c0cf7
rsi35 <ricardo.silva@sky.uk>
2023-12-06 08:17:17 +0000

    day 6

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,9 @@
 import puzzles.puzzle5
 import puzzles.puzzle5dot1
+import puzzles.puzzle6
+import puzzles.puzzle6dot1
 
 fun main() {
-    println(puzzle5())
-    println(puzzle5dot1())
+    println(puzzle6())
+    println(puzzle6dot1())
 }
--- src/main/kotlin/puzzles/Puzzle6.kt
+++ src/main/kotlin/puzzles/Puzzle6.kt
@@ -4,6 +4,31 @@ import java.io.File
 
 private val lines = File("input/puzzle6/input.txt").readLines()
 
-fun puzzle6(): String = run run@{ lines[0].foldIndexed("") {i, acc, c -> if(acc.toSet().size == 4) return@run "$i" else acc.takeLast(3) + c}}
+fun puzzle6(): Int {
+    val times = "\\d+".toRegex().findAll(lines[0]).map { it.value.toInt() }.toList()
+    val distances = "\\d+".toRegex().findAll(lines[1]).map { it.value.toInt() }.toList()
 
-fun puzzle6dot1(): String = run run@{ lines[0].foldIndexed("") {i, acc, c -> if(acc.toSet().size == 14) return@run "$i" else acc.takeLast(13) + c}}
+    return times.foldIndexed(1) { index, acc, t ->
+        var winning = 0
+        for (hold in 0..t) {
+            val traveledDistance = (t - hold) * hold
+            if (traveledDistance > distances[index]) winning++
+        }
+
+        acc * winning
+    }
+}
+
+fun puzzle6dot1(): Int {
+    val time = "\\d+".toRegex().findAll(lines[0]).fold("") { acc, matchResult -> acc + matchResult.value }.toLong()
+    val distance = "\\d+".toRegex().findAll(lines[1]).fold("") { acc, matchResult -> acc + matchResult.value }.toLong()
+
+    var winning = 0
+    for (hold in 0..time) {
+        val traveledDistance = (time - hold) * hold
+        if (traveledDistance > distance) winning++
+    }
+
+    return winning
+}
+
