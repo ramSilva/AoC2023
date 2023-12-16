20d516481276f3c198f9c9cf226ad563a6687177
rsi35 <ricardo.silva@sky.uk>
2023-12-16 20:31:26 +0000

    simplify 16

--- src/main/kotlin/puzzles/Puzzle16.kt
+++ src/main/kotlin/puzzles/Puzzle16.kt
@@ -8,7 +8,7 @@ private val lines = File("input/puzzle16/input.txt").readLines()
 fun Pair<Int, Int>.sum(other: Pair<Int, Int>): Pair<Int, Int> =
     Pair(this.first + other.first, this.second + other.second)
 
-fun puzzle16(): Int {
+fun puzzle16(initialPosition: Pair<Int, Int> = Pair(0, 0), initialDirection: Pair<Int, Int> = Pair(1, 0)): Int {
     val mirrors = mutableMapOf<Pair<Int, Int>, Char>()
 
     val maxX = lines[0].length
@@ -20,10 +20,7 @@ fun puzzle16(): Int {
         }
     }
 
-    // store a list of light rays. Each light ray is composed by two pairs: Position and Direction.
-    // The direction is a unit vector
-    var lightInfo = mutableListOf<Pair<Pair<Int, Int>, Pair<Int, Int>>>()
-    lightInfo.add(Pair(Pair(0, 0), Pair(1, 0)))
+    var lightInfo = mutableListOf(Pair(initialPosition, initialDirection))
 
     val energizedSquares = mutableSetOf<Pair<Int, Int>>()
     val loopDetector3000 = mutableListOf<Pair<Pair<Int, Int>, Pair<Int, Int>>>()
@@ -36,81 +33,21 @@ fun puzzle16(): Int {
             energizedSquares.add(it.first)
 
             val newDirections = when (mirrors[it.first]) {
-                '/' -> {
-                    when (it.second) {
-                        Pair(1, 0) -> {
-                            listOf(Pair(0, -1))
-                        }
+                '/' -> listOf(Pair(it.second.second * -1, it.second.first * -1))
 
-                        Pair(-1, 0) -> {
-                            listOf(Pair(0, 1))
-                        }
+                '\\' -> listOf(Pair(it.second.second, it.second.first))
 
-                        Pair(0, 1) -> {
-                            listOf(Pair(-1, 0))
-                        }
-
-                        Pair(0, -1) -> {
-                            listOf(Pair(1, 0))
-                        }
-
-                        else -> {
-                            listOf(it.second)
-                        }
-                    }
+                '|' -> when (it.second) {
+                    Pair(1, 0), Pair(-1, 0) -> listOf(Pair(0, 1), Pair(0, -1))
+                    else -> listOf(it.second)
                 }
 
-                '\\' -> {
-                    when (it.second) {
-                        Pair(1, 0) -> {
-                            listOf(Pair(0, 1))
-                        }
-
-                        Pair(-1, 0) -> {
-                            listOf(Pair(0, -1))
-                        }
-
-                        Pair(0, 1) -> {
-                            listOf(Pair(1, 0))
-                        }
-
-                        Pair(0, -1) -> {
-                            listOf(Pair(-1, 0))
-                        }
-
-                        else -> {
-                            listOf(it.second)
-                        }
-                    }
+                '-' -> when (it.second) {
+                    Pair(0, 1), Pair(0, -1) -> listOf(Pair(-1, 0), Pair(1, 0))
+                    else -> listOf(it.second)
                 }
 
-                '|' -> {
-                    when (it.second) {
-                        Pair(1, 0), Pair(-1, 0) -> {
-                            listOf(Pair(0, 1), Pair(0, -1))
-                        }
-
-                        else -> {
-                            listOf(it.second)
-                        }
-                    }
-                }
-
-                '-' -> {
-                    when (it.second) {
-                        Pair(0, 1), Pair(0, -1) -> {
-                            listOf(Pair(-1, 0), Pair(1, 0))
-                        }
-
-                        else -> {
-                            listOf(it.second)
-                        }
-                    }
-                }
-
-                else -> {
-                    listOf(it.second)
-                }
+                else -> listOf(it.second)
             }
 
             newDirections.forEach { dir ->
@@ -131,21 +68,12 @@ fun puzzle16(): Int {
 }
 
 fun puzzle16dot1(): Int {
-    val mirrors = mutableMapOf<Pair<Int, Int>, Char>()
-
     val maxX = lines[0].length
     val maxY = lines.size
-
-    lines.forEachIndexed { y, s ->
-        s.forEachIndexed { x, c ->
-            if (c != '.') mirrors[Pair(x, y)] = c
-        }
-    }
 
     var maxEnergy = 0
     for (x in 0 until maxX) {
         for (y in 0 until maxY) {
-            val initialPosition = Pair(x, y)
             val initialDirections = mutableListOf<Pair<Int, Int>>()
             if (x == 0)
                 initialDirections.add(Pair(1, 0))
@@ -157,114 +85,7 @@ fun puzzle16dot1(): Int {
                 initialDirections.add(Pair(0, -1))
 
             initialDirections.forEach {
-                // store a list of light rays. Each light ray is composed by two pairs: Position and Direction.
-                // The direction is a unit vector
-                var lightInfo = mutableListOf<Pair<Pair<Int, Int>, Pair<Int, Int>>>()
-
-                lightInfo.add(Pair(initialPosition, it))
-
-                val energizedSquares = mutableSetOf<Pair<Int, Int>>()
-                val loopDetector3000 = mutableListOf<Pair<Pair<Int, Int>, Pair<Int, Int>>>()
-
-                while (lightInfo.isNotEmpty()) {
-                    val updatedLightInfo = lightInfo.toMutableList()
-                    lightInfo.forEach {
-                        updatedLightInfo.remove(it)
-                        loopDetector3000.add(it)
-                        energizedSquares.add(it.first)
-
-                        val newDirections = when (mirrors[it.first]) {
-                            '/' -> {
-                                when (it.second) {
-                                    Pair(1, 0) -> {
-                                        listOf(Pair(0, -1))
-                                    }
-
-                                    Pair(-1, 0) -> {
-                                        listOf(Pair(0, 1))
-                                    }
-
-                                    Pair(0, 1) -> {
-                                        listOf(Pair(-1, 0))
-                                    }
-
-                                    Pair(0, -1) -> {
-                                        listOf(Pair(1, 0))
-                                    }
-
-                                    else -> {
-                                        listOf(it.second)
-                                    }
-                                }
-                            }
-
-                            '\\' -> {
-                                when (it.second) {
-                                    Pair(1, 0) -> {
-                                        listOf(Pair(0, 1))
-                                    }
-
-                                    Pair(-1, 0) -> {
-                                        listOf(Pair(0, -1))
-                                    }
-
-                                    Pair(0, 1) -> {
-                                        listOf(Pair(1, 0))
-                                    }
-
-                                    Pair(0, -1) -> {
-                                        listOf(Pair(-1, 0))
-                                    }
-
-                                    else -> {
-                                        listOf(it.second)
-                                    }
-                                }
-                            }
-
-                            '|' -> {
-                                when (it.second) {
-                                    Pair(1, 0), Pair(-1, 0) -> {
-                                        listOf(Pair(0, 1), Pair(0, -1))
-                                    }
-
-                                    else -> {
-                                        listOf(it.second)
-                                    }
-                                }
-                            }
-
-                            '-' -> {
-                                when (it.second) {
-                                    Pair(0, 1), Pair(0, -1) -> {
-                                        listOf(Pair(-1, 0), Pair(1, 0))
-                                    }
-
-                                    else -> {
-                                        listOf(it.second)
-                                    }
-                                }
-                            }
-
-                            else -> {
-                                listOf(it.second)
-                            }
-                        }
-
-                        newDirections.forEach { dir ->
-                            val newPosition = it.first.sum(dir)
-                            val newLightInfo = Pair(newPosition, dir)
-                            if (newPosition.first < maxX && newPosition.second < maxY && newPosition.first >= 0 && newPosition.second >= 0
-                                && !loopDetector3000.contains(newLightInfo)
-                            ) {
-                                updatedLightInfo.add(newLightInfo)
-                            }
-
-                        }
-                    }
-                    lightInfo = updatedLightInfo.toMutableList()
-                }
-                maxEnergy = maxOf(energizedSquares.size, maxEnergy)
+                maxEnergy = maxOf(puzzle16(Pair(x, y), it), maxEnergy)
             }
         }
     }
